a451ece87feb48e7b117d3432829698a8aaf41ae
Kevin Woo <3469532+kevinawoo@users.noreply.github.com>
2025-04-15 18:15:07 -0700

    cleanup

--- print-payload-size/helloworld.go
+++ print-payload-size/helloworld.go
@@ -8,36 +8,37 @@ import (
 )
 
 // Workflow is a Hello World workflow definition.
-func Workflow(ctx workflow.Context, name string) (string, error) {
+func Workflow(ctx workflow.Context, payload string) (string, error) {
 	ao := workflow.ActivityOptions{
 		StartToCloseTimeout: 10 * time.Second,
 	}
 	ctx = workflow.WithActivityOptions(ctx, ao)
 
 	logger := workflow.GetLogger(ctx)
-	logger.Info("HelloWorld workflow started", "name", name)
+	logger.Info("HelloWorld workflow started")
 
 	var result string
-	name = RandStringBytes(1_000_000 * 1)
+
+	payload = RandStringBytes(1_000_000 * 1) // generates a random 1 MB payload
 
 	// Notice that if we call these ExecuteActivity calls are asynchronous, the SDK will batch them together to the Server
 	// In this case, the gRPC interceptor will print out
 	//		RespondWorkflowTaskCompleted payload size 3.0 MB len(commands) 3
 	//
 	// The `.Get()` command is a synchronous, which the SDK will wait for a result
-	workflow.ExecuteActivity(ctx, Activity, name)
-	workflow.ExecuteActivity(ctx, Activity, name)
-	err := workflow.ExecuteActivity(ctx, Activity, name).Get(ctx, &result)
+	workflow.ExecuteActivity(ctx, Activity, payload)
+	workflow.ExecuteActivity(ctx, Activity, payload)
+	err := workflow.ExecuteActivity(ctx, Activity, payload).Get(ctx, &result)
 	if err != nil {
 		logger.Error("Activity failed.", "Error", err)
 		return "", err
 	}
 
-	logger.Info("HelloWorld workflow completed.", "result", result)
+	logger.Info("HelloWorld workflow completed")
 
 	return result, nil
 }
 
-func Activity(ctx context.Context, name string) (string, error) {
+func Activity(ctx context.Context, input string) (string, error) {
 	return RandStringBytes(1_000_000 * 2), nil
 }
--- print-payload-size/worker/main.go
+++ print-payload-size/worker/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	clientLogger "go.temporal.io/sdk/log"
 	"google.golang.org/grpc"
 	"log"
 	"print_payload_size"
@@ -15,7 +13,6 @@ import (
 func main() {
 	// The client and worker are heavyweight objects that should be created once per process.
 	c, err := client.Dial(client.Options{
-		Logger: &simpleLogger{},
 		ConnectionOptions: client.ConnectionOptions{
 			DialOptions: []grpc.DialOption{
 				grpc.WithStatsHandler(grpc_stats.New()),
@@ -36,25 +33,4 @@ func main() {
 	if err != nil {
 		log.Fatalln("Unable to start worker", err)
 	}
-}
-
-// simpleLogger	drops keyvals because they contain the large payloads which we don't want printed out
-type simpleLogger struct{}
-
-var _ = (*clientLogger.Logger)(nil)
-
-func (simpleLogger) Debug(msg string, keyvals ...interface{}) {
-	fmt.Printf("DEBUG: " + msg + "\n")
-}
-
-func (simpleLogger) Info(msg string, keyvals ...interface{}) {
-	fmt.Printf("INFO: " + msg + "\n")
-}
-
-func (simpleLogger) Warn(msg string, keyvals ...interface{}) {
-	fmt.Printf("WARN: " + msg + "\n")
-}
-
-func (simpleLogger) Error(msg string, keyvals ...interface{}) {
-	fmt.Printf("ERROR: " + msg + "\n")
 }
